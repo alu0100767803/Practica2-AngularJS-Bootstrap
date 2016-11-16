afc31a356ec41e6f586adb8e5cfd6062466cfcfd
Jorge Alonso <alu0100767803@ull.edu.es>
2016-11-16 16:39:46 +0000

    cambios

--- js/comprobacion.js
+++ js/comprobacion.js
@@ -10,3 +10,15 @@ function validar(email) {
 
 	return error;    
 }
+
+window.onload = function(){
+	$('#myTabs a').click(function (e) {
+  		e.preventDefault()
+	  $(this).tab('show')
+	})
+
+	$('#myTabs a[href="#profile"]').tab('show') // Select tab by name
+	$('#myTabs a:first').tab('show') // Select first tab
+	$('#myTabs a:last').tab('show') // Select last tab
+	$('#myTabs li:eq(2) a').tab('show') // Select third tab (0-indexed)
+}
